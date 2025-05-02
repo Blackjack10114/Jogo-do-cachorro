3b4e9af9627ac91031ae030a68edf641d20c69b4
Blackjack10114 <gsl10114a@gmail.com>
2025-05-02 18:03:05 -0300

    Empacado com power up na hud :(

--- Assets/Scripts/Power_Up_UI.cs
+++ Assets/Scripts/Power_Up_UI.cs
@@ -1,15 +1,19 @@
+using System.Collections;
 using UnityEngine;
 
 public class Power_Up_UI : MonoBehaviour
 {
     private Power_Up_Coletavel bool_script;
     public GameObject Power_Up;
-    private PowerUp duracao;
+    public GameObject doguinho;
+    private PlayerMov duracao;
+    bool turbo_ativado;
+    bool gourmet_ativado;
     void Start()
     {
         GetComponent<Renderer>().enabled = false;
         bool_script = Power_Up.GetComponent<Power_Up_Coletavel>();
-        duracao = Power_Up.GetComponent<PowerUp>();
+        duracao = doguinho.GetComponent<PlayerMov>();
     }
 
     // Update is called once per frame
@@ -19,9 +23,36 @@ public class Power_Up_UI : MonoBehaviour
         {
             GetComponent<Renderer>().enabled = true;
         }
-        if (duracao.duration <= 0)
+        if (duracao.isTurboActive == true)
         {
-            GetComponent<Renderer>().enabled = false;
+            turbo_ativado = true;
         }
+        if (turbo_ativado == true)
+        {
+            if (duracao.isTurboActive == false)
+            {
+                StartCoroutine(DelayVerificaçãotempo());
+            }
+        }
+        if (duracao.isGourmetActive == true)
+        {
+            gourmet_ativado = true;
+        }
+        if (gourmet_ativado == true)
+        {
+            if (duracao.isGourmetActive == false)
+            {
+                StartCoroutine(DelayVerificaçãotempo());
+            }
+        }
+    }
+    
+    
+    private IEnumerator DelayVerificaçãotempo()
+    {
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<Renderer>().enabled = false;
+        turbo_ativado = false;
+        gourmet_ativado = false;
     }
 }
