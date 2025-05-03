1e5048d0e2ba69fbedc21604c3f7f3ed5a76305f
MaisUmHugo <hugo.marcos003@gmail.com>
2025-05-03 13:31:52 -0300

    Raiz com duas opções oscilante(frente e trás) e girando completo
    - Raiz com duas opções oscilante(frente e trás) e girando completo (circulo)
    - Coloquei o passaro spawner como prefab e na pasta prefab

--- Assets/Scripts/Raiz_Rotatoria.cs
+++ Assets/Scripts/Raiz_Rotatoria.cs
@@ -2,22 +2,42 @@ using UnityEngine;
 
 public class Raiz_Rotatoria : MonoBehaviour
 {
-    public float amplitude = 45f;
+    public enum TipoRotacao { Oscilacao, RotacaoCompleta }
+    public TipoRotacao tipo = TipoRotacao.Oscilacao;
+
+    [Header("Configurações Gerais")]
     public float velocidade = 2f;
-    private float anguloinicial;
-    
+
+    [Header("Oscilação")]
+    public float amplitude = 45f;
+    private float anguloInicial;
+
     void Start()
     {
-        anguloinicial = transform.rotation.eulerAngles.z;
-  
+        anguloInicial = transform.rotation.eulerAngles.z;
     }
 
-
-    
     void Update()
     {
+        switch (tipo)
+        {
+            case TipoRotacao.Oscilacao:
+                Oscilar();
+                break;
+            case TipoRotacao.RotacaoCompleta:
+                RotacionarCompleto();
+                break;
+        }
+    }
+
+    void Oscilar()
+    {
         float angulo = amplitude * Mathf.Sin(Time.time * velocidade);
-        transform.rotation = Quaternion.Euler(0, 0, anguloinicial + angulo);
+        transform.rotation = Quaternion.Euler(0, 0, anguloInicial + angulo);
+    }
+
+    void RotacionarCompleto()
+    {
+        transform.Rotate(0, 0, velocidade * Time.deltaTime * 360f); // 360f para uma volta por segundo com velocidade = 1
     }
 }
-
