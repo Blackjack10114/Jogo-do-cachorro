4408522bbd6e7106abab02131d1174651928d7dd
MaisUmHugo <hugo.marcos003@gmail.com>
2025-05-09 08:50:17 -0300

    Volume salva e não reseta para o volume original

--- Assets/Scripts/Controlador_Som.cs
+++ Assets/Scripts/Controlador_Som.cs
@@ -1,32 +1,40 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+
 public class Controlador_Som : MonoBehaviour
 {
     private bool estadoSom = true;
-    [SerializeField]private AudioSource fundoMusical;
 
-    [SerializeField]private Sprite somLigadoSprite;
+    [SerializeField] private AudioSource fundoMusical;
+    [SerializeField] private Sprite somLigadoSprite;
     [SerializeField] private Sprite somDesligadoSprite;
-
     [SerializeField] private Image muteImage;
-   public void LigarDesligarSom()
+    [SerializeField] private Slider volumeSlider; // ← Atribua o Slider no Inspector
+
+    void Start()
+    {
+        float volumeSalvo = PlayerPrefs.GetFloat("VolumeMusica", 1f);
+        fundoMusical.volume = volumeSalvo;
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = volumeSalvo;
+        }
+    }
+
+    public void LigarDesligarSom()
     {
         estadoSom = !estadoSom;
         fundoMusical.enabled = estadoSom;
 
-        if (estadoSom)
-        {
-            muteImage.sprite = somLigadoSprite;
-        }
-        else
-        {
-            muteImage.sprite = somDesligadoSprite;
-        }
+        muteImage.sprite = estadoSom ? somLigadoSprite : somDesligadoSprite;
     }
+
     public void VolumeMusical(float value)
     {
         fundoMusical.volume = value;
+        PlayerPrefs.SetFloat("VolumeMusica", value);
+        PlayerPrefs.Save();
     }
 }
