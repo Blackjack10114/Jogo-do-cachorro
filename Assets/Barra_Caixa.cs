7ab6ee14ab638499757463039a305035026c460d
Blackjack10114 <gsl10114a@gmail.com>
2025-05-13 15:14:54 -0300

    Barra de vida da caixa funcionando

--- Assets/Barra_Caixa.cs
+++ Assets/Barra_Caixa.cs
@@ -6,12 +6,22 @@ public class Barra_Caixa : MonoBehaviour
     private GameObject Player;
     public Color CorMetade, CorFinal;
     public Image Barravida;
+    public float Vida, VidaMax;
+    private Dano Vervida;
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
+        Vervida = Player.GetComponent<Dano>();
     }
 
     void Update()
     {
+        Vida = Vervida.pv;
+        if (Vida < 0)
+        {
+            Vida = 0;
+        }
+        Barravida.fillAmount = Vida / VidaMax;
+        Debug.Log(Vervida.pv);
     }
 }
