35785f3174ee0b6312edeeaad6ca4d34a6ac5050
Blackjack10114 <gsl10114a@gmail.com>
2025-05-13 21:08:31 -0300

    Power ups na UI
    só falta pulo duplo

---
+++ Assets/Up_UI_Teste.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.Collections;
+
+public class Up_UI_Teste : MonoBehaviour
+{
+    private PlayerMov duracao;
+    private GameObject Player;
+    bool turbo_ativado; 
+    bool turbo_insta, gourmet_insta, pulo_insta, bolha_insta;
+    bool gourmet_ativado;
+    bool bolha_ativada;
+    bool pulo_duplo_ativado;
+    private GameObject TurboPrefab = null;
+    private GameObject GourmetPrefab = null;
+    private GameObject BolhaPrefab = null;
+    private GameObject PuloPrefab = null;
+    private Dano Bolha;
+    private GameObject objetoreferencia;
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+        duracao = Player.GetComponent<PlayerMov>();
+        TurboPrefab = Resources.Load<GameObject>("PowerUp_Ração_Turbo_UI");
+        GourmetPrefab = Resources.Load<GameObject>("PowerUp_Racao_Dourada_UI");
+        BolhaPrefab = Resources.Load<GameObject>("PowerUp_Bolha_UI");
+        Bolha = Player.GetComponent<Dano>();
+        objetoreferencia = this.gameObject;
+    }
+    private void Update()
+    {
+       //verificação turbo
+        if (duracao.isTurboActive == true && TurboPrefab != null && !turbo_insta)
+        {
+            turbo_ativado = true;
+            TurboPrefab = Instantiate(TurboPrefab, this.transform.position, Quaternion.identity);
+            TurboPrefab.transform.parent = this.transform;
+            turbo_insta = true;
+        }
+        if (turbo_ativado == true)
+        {
+            if (duracao.isTurboActive == false)
+            {
+                Destroy(TurboPrefab);
+                turbo_ativado = false;
+            }
+        }
+        //Verificaçao gourmet
+        if (duracao.isGourmetActive == true && GourmetPrefab != null && !gourmet_insta)
+        {
+            gourmet_ativado = true;
+            GourmetPrefab = Instantiate(GourmetPrefab, this.transform.position, Quaternion.identity);
+            GourmetPrefab.transform.parent = this.transform;
+            gourmet_insta = true;
+        }
+        if (gourmet_ativado == true)
+        {
+            if (duracao.isGourmetActive == false)
+            {
+                Destroy(GourmetPrefab);
+                gourmet_ativado = false;
+            }
+        }
+        if (Bolha.isInvincible == true && !bolha_insta)
+        {
+            bolha_ativada = true;
+            Vector3 offset = new Vector3(-8f, 0f, 0f);
+            Vector3 spawnPosition = objetoreferencia.transform.position + offset;
+            BolhaPrefab = Instantiate(BolhaPrefab, spawnPosition, Quaternion.identity);
+            BolhaPrefab.transform.parent = this.transform;
+            bolha_insta = true;
+        }
+        if (bolha_ativada == true)
+        {
+            if (Bolha.isInvincible == false)
+            {
+                Destroy(BolhaPrefab);
+                bolha_ativada = false;
+            }
+        }
+    }
+}
