68f3cf9ab7630d693e04c456efb63b43ff0e55e5
Blackjack10114 <gsl10114a@gmail.com>
2025-05-17 15:06:05 -0300

    Barra de stamina 100% funcional

--- Assets/Scripts/Stamina_Barra.cs
+++ Assets/Scripts/Stamina_Barra.cs
@@ -3,16 +3,25 @@ using UnityEngine.UI;
 
 public class Stamina_Barra : MonoBehaviour
 {
-    private GameObject Player;
-    public Color CorMetade, CorFinal;
+    private GameObject Player, fundostamina;
+    public Color CorMetade, CorFinal, cormaismetade;
     public Image Barrastamina;
-    public float Stamina, StaminaMax;
+    public float Stamina;
+    private float StaminaMax = 100;
     private PlayerMov VerStamina;
     private Vector3 offset = new Vector3(0f,4.5f,0f);
+    private SpriteRenderer SrFundo;
+    private Image SrBarra;
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         VerStamina = Player.GetComponent<PlayerMov>();
+        CorMetade.a = 1f;
+        CorFinal.a = 1f;
+        cormaismetade.a = 1f;
+        fundostamina = GameObject.Find("Fundo_Stamina");
+        SrFundo = fundostamina.GetComponent<SpriteRenderer>();
+        SrBarra = this.GetComponent<Image>();
     }
 
     void Update()
@@ -22,15 +31,37 @@ public class Stamina_Barra : MonoBehaviour
         {
             Stamina = 0;
         }
-        if (VerStamina.stamina <= VerStamina.stamina / 2)
+        verificarcor();
+        VerificarVisivel();
+        Barrastamina.fillAmount = Stamina / StaminaMax;
+        transform.position = Player.transform.position + offset;
+    }
+    private void verificarcor()
+    {
+        if (VerStamina.stamina <= StaminaMax / 2)
         {
             Barrastamina.color = CorMetade;
         }
-        if (VerStamina.stamina <= VerStamina.stamina / 4)
+        if (VerStamina.stamina <= StaminaMax / 4)
         {
             Barrastamina.color = CorFinal;
         }
-        Barrastamina.fillAmount = Stamina / StaminaMax;
-        transform.position = Player.transform.position + offset;
+        if (VerStamina.stamina >= StaminaMax / 2)
+        {
+            Barrastamina.color = cormaismetade;
+        }
+    }
+    private void VerificarVisivel()
+    {
+        if (VerStamina.stamina >= 100)
+        {
+            SrFundo.enabled = false;
+            SrBarra.enabled = false;
+        }
+        else
+        {
+            SrFundo.enabled = true;
+            SrBarra.enabled = true;
+        }
     }
 }
