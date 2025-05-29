6fd73969c60969872ee8ad14c1d05748050791b3
Blackjack10114 <gsl10114a@gmail.com>
2025-05-29 16:35:16 -0300

    Tela de meta centralizada e seta girando em tempo real

--- Assets/Scripts/Seta_Consumidor.cs
+++ Assets/Scripts/Seta_Consumidor.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Seta_Consumidor : MonoBehaviour
 {
-    Vector3 Pos_seta, Pos_consumidor, Calculo_Pos;
     public Vector3 offset = new Vector3(4f, 0f, 0f);
-    GameObject Consumidor, Player;
+    private GameObject Consumidor, Player;
+
     void Start()
     {
         Player = GameObject.FindWithTag("Player");
@@ -13,27 +13,13 @@ public class Seta_Consumidor : MonoBehaviour
 
     void Update()
     {
+
         transform.position = Player.transform.position + offset;
-        Pos_seta = Player.transform.position;
-        Pos_consumidor = Consumidor.transform.position;
-        Calculo_Pos = Pos_consumidor - Pos_seta;
-        if (Calculo_Pos.x > 0 && Calculo_Pos.y <= 50 && Calculo_Pos.y >= -50)
-        {
-            GetComponent<SpriteRenderer>().flipX = true;
-            this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        }
-        if (Calculo_Pos.x < 0 && Calculo_Pos.y <= 50 && Calculo_Pos.y >= -50)
-        {
-            GetComponent<SpriteRenderer>().flipX = false;
-            this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        }
-        if (Calculo_Pos.y >= 50)
-        {
-            this.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
-        }
-        if (Calculo_Pos.y <= -50)
-        {
-            this.transform.rotation = Quaternion.Euler(0f, 0f, -90f);
-        }
+
+        Vector3 direction = Consumidor.transform.position - Player.transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 }
