7a1dec5f22d81c0a43a91a2889ea0d18ca588898
MaisUmHugo <hugo.marcos003@gmail.com>
2025-05-31 18:14:56 -0300

    Aparece panel de tutorial na primeira vez que joga

--- Assets/Scripts/MenuPrincipalMangement.cs
+++ Assets/Scripts/MenuPrincipalMangement.cs
@@ -3,59 +3,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-
 public class MenuPrincipalMangement : MonoBehaviour
 {
-    [SerializeField]private string nomeDoLevelDeJogo;
-    [SerializeField]private GameObject painelMenuInicial;
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject painelOpcoes;
     [SerializeField] private string CenaRetry;
     [SerializeField] private GameObject painelConfirmacao;
     [SerializeField] private GameObject painelTutorial;
 
-
     private System.Action acaoConfirmada;
+
     public void Jogar()
     {
-        AbrirPainelTutorial();
-        
+        // Exibe a pergunta do tutorial apenas se for a primeira vez
+        if (!PlayerPrefs.HasKey("JaViuTutorialPergunta"))
+        {
+            painelTutorial.SetActive(true);
+        }
+        else
+        {
+            SceneManager.LoadScene("CenaSelecaoFase");
+        }
     }
+
     public void Retry()
     {
         SceneManager.LoadScene(CenaRetry);
     }
+
     public void AbrirOpcoes()
     {
         painelMenuInicial.SetActive(false);
         painelOpcoes.SetActive(true);
     }
+
     public void FecharOpcoes()
     {
         painelOpcoes.SetActive(false);
         painelMenuInicial.SetActive(true);
     }
+
     public void SairJogo()
     {
         MostrarConfirmacao(() =>
         {
             Debug.Log("Sair do Jogo");
-        Application.Quit();
+            Application.Quit();
         });
     }
+
     public void MenuPrincipal()
     {
         SceneManager.LoadScene("MenuPrincipal");
     }
-    
-    public void ProximaFase()
-    {
-        SceneManager.LoadScene("Fase_Alien_02");
-    }
+
     private void MostrarConfirmacao(System.Action acao)
     {
         painelConfirmacao.SetActive(true);
         acaoConfirmada = acao;
     }
+
     public void BotaoConfirmarSim()
     {
         acaoConfirmada?.Invoke();
@@ -75,21 +83,19 @@ public class MenuPrincipalMangement : MonoBehaviour
     {
         SceneManager.LoadScene("Tutorial");
     }
-    public void AbrirPainelTutorial()
-    {
-        //painelMenuInicial.SetActive(false);
-        painelTutorial.SetActive(true);
-    }
+
     public void TutorialSim()
     {
+        PlayerPrefs.SetInt("JaViuTutorialPergunta", 1); 
+        PlayerPrefs.Save();
         SceneManager.LoadScene("Tutorial");
     }
 
     public void TutorialNao()
     {
+        PlayerPrefs.SetInt("JaViuTutorialPergunta", 1); 
+        PlayerPrefs.Save();
         painelTutorial.SetActive(false);
         SceneManager.LoadScene("CenaSelecaoFase");
     }
-
-
 }
