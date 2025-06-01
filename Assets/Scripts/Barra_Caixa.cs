a99271ad21bdbe0b23538af299719083f6fde4ca
Blackjack10114 <gsl10114a@gmail.com>
2025-06-01 14:53:43 -0300

    Power ups com prefabs e barra de vida mudando de cor ao chegar com vida baixa

--- Assets/Scripts/Barra_Caixa.cs
+++ Assets/Scripts/Barra_Caixa.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 public class Barra_Caixa : MonoBehaviour
 {
     private GameObject Player;
-    public Color CorMetade, CorFinal;
+    public Color CorMuitavida, CorPoucavida;
     public Image Barravida;
     public float Vida, VidaMax;
     private Dano Vervida;
@@ -12,6 +12,8 @@ public class Barra_Caixa : MonoBehaviour
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         Vervida = Player.GetComponent<Dano>();
+        CorPoucavida = new Color(202 / 255f, 17 / 255f, 46 / 255f, 1f);
+        CorMuitavida = new Color(255 / 255f, 200 / 255f, 37 / 255f, 1f);
     }
 
     void Update()
@@ -22,5 +24,17 @@ public class Barra_Caixa : MonoBehaviour
             Vida = 0;
         }
         Barravida.fillAmount = Vida / VidaMax;
+        verificarcorvida();
+    }
+    private void verificarcorvida()
+    {
+        if (Vervida.pv <= 40)
+        {
+            Barravida.color = CorPoucavida;
+        }
+        else
+        {
+            Barravida.color = CorMuitavida;
+        }
     }
 }
