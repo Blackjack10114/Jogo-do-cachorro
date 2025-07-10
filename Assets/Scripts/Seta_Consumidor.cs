33170acb48fbb89985bfaf3557c24d79d7acee31
Blackjack10114 <gsl10114a@gmail.com>
2025-07-10 18:53:00 -0300

    Possibilidade de desabilitar seta

--- Assets/Scripts/Seta_Consumidor.cs
+++ Assets/Scripts/Seta_Consumidor.cs
@@ -4,16 +4,33 @@ public class Seta_Consumidor : MonoBehaviour
 {
     public Vector3 offset = new Vector3(4f, 0f, 0f);
     private GameObject Consumidor, Player;
-
+    private bool visivel;
     void Start()
     {
+        visivel = true;
         Player = GameObject.FindWithTag("Player");
         Consumidor = GameObject.FindWithTag("Consumidor");
     }
 
     void Update()
     {
-
+        if (visivel)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("invisivel");
+                GetComponent<SpriteRenderer>().enabled = false;
+                visivel = false;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                GetComponent<SpriteRenderer>().enabled = true;
+                visivel = true;
+            }
+        }
         transform.position = Player.transform.position + offset;
 
         Vector3 direction = Consumidor.transform.position - Player.transform.position;
